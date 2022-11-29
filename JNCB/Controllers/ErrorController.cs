d7a175595eb1d3d29fa1364a37d484ccbf01f2d5
Philmore Foster <fosterphilmore@outlook.com>
2022-11-29 09:30:34 -0500

    Added the error page handling 404 and favicon

---
+++ JNCB/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EC2_1701497.Controllers
+{
+    public class ErrorController : Controller
+    {
+               
+            [Route("Error/{statusCode}")]
+            public IActionResult HttpStatusCodeHandler(int statuscode)
+           {
+                switch(statuscode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, COuld not find the page you requested ";
+                    break;
+            }
+
+            return View("Not Found");
+
+            }
+           
+    }
+}
